1a265dfe9b30e5428f0a3c3dae5b069211d7dc6b
joaomelga <joaolucasfm@gmail.com>
2018-04-20 11:46:22 -0300

    - Criação checkout.php

---
+++ checkout.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Checkout Mirror Fashion</title>
+    <meta name="viewport" content="width=device-width">
+  </head>
+
+  <body>
+    <h1>Ótima escolha!</h1>
+    <p>Obrigado por comprar na Mirror Fashion!
+    Preencha seus dados para efetivar a compra.</p>
+  </body>
+</html>
