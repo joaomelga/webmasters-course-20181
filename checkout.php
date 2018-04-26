5d1fe20dec91d28c1fb002696d8b77fd43bcebac
joaomelga <joaolucasfm@gmail.com>
2018-04-25 22:44:25 -0300

    - Primeira aula com PHP - Passagem de parâmetros com PHP

--- checkout.php
+++ checkout.php
@@ -3,12 +3,29 @@
   <head>
     <meta charset="UTF-8">
     <title>Checkout Mirror Fashion</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
     <meta name="viewport" content="width=device-width">
   </head>
 
   <body>
-    <h1>Ótima escolha!</h1>
-    <p>Obrigado por comprar na Mirror Fashion!
-    Preencha seus dados para efetivar a compra.</p>
+    <div class="jumbotron">    
+      <div class="container">
+        <h1>Ótima escolha!</h1>
+        <p>Obrigado por comprar na Mirror Fashion!
+        Preencha seus dados para efetivar a compra.</p>
+
+        <h2>Sua compra</h2>
+
+        <dl>
+          <dt>Cor</dt>
+          <dd><?php print $_GET['cor'] ?></dd>
+
+          <dt>Tamanho</dt>
+          <dd><?php print $_GET['tamanho'] ?></dd>
+        </dl>
+
+      </div>
+
+    </div>
   </body>
 </html>
--- produto.php
+++ produto.php
@@ -9,21 +9,21 @@
         <h1>Fuzzy Cardigan</h1>
         <p>por apenas R$ 129,00</p>
 
-        <form>
+        <form action="checkout.php" method="GET">
           <fieldset class="cores">
             <legend>Escolha a cor:</legend>
 
-            <input type="radio" name="cor" value="verde" id="verde" checked>
+            <input type="radio" name="cor" value="Verde" id="verde" checked>
             <label for="verde">
               <img src="img/produtos/foto2-verde.png" alt="verde">
             </label>
 
-            <input type="radio" name="cor" value="rosa" id="rosa">
+            <input type="radio" name="cor" value="Rosa" id="rosa">
             <label for="rosa">
               <img src="img/produtos/foto2-rosa.png" alt="rosa">
             </label>
 
-            <input type="radio" name="cor" value="azul" id="azul">
+            <input type="radio" name="cor" value="Azul" id="azul">
             <label for="azul">
               <img src="img/produtos/foto2-azul.png" alt="azul">
             </label>
