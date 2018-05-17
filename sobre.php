d3f0c50f721f8401dfd361191eb2505b09ddda8e
João Melga <joaolucasfm@gmail.com>
2018-05-16 23:02:43 -0300

    Teste02

--- sobre.php
+++ sobre.php
@@ -12,6 +12,7 @@
 		<h1>A Mirror Fashion</h1>
 		
 		<p>
+			A <strong>Mirror Fashion</strong> é a maior comércio eletrônico no segmento de moda em todo o mundo. 
 			Fundada em 1932, possui filiais em 124 países, sendo líder de mercado com mais de 90% de 
 			participação em 118 deles.
 
@@ -23,8 +24,10 @@
 			<figcaption>Centro de distribuição da Mirror Fashion</figcaption>
 		</figure>
 		
+		João Teste LALALA
+		
 		<p>
-			De lá, saem 48 aviões que distribuem nossos produtos às casas do mundo todo. Nosso centro de distribuição:
+			
 
 			Compre suas roupas e acessórios na Mirror Fashion. Acesse nossa loja ou entre em contato 
 			se tiver dúvidas. Conheça também nossa <a href="#historia">história</a> e nossos <a href="#diferenciais">diferenciais</a>.
